81be943bab715f7ef3a455d337e07c766e29182f
rafaela3456 <115078515+rafaela3456@users.noreply.github.com>
2024-08-28 07:41:25 -0300

    uy

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const caixaprincipal = document.querySelector (".caixa-principal");
 const caixaprincipal = document.querySelector (".caixa-principal");
+const caixaprincipal = document.querySelector (".caixa-alternativa");
+const caixaprincipal = document.querySelector (".caixa-resultado");
+const caixaprincipal = document.querySelector (".texto-resu");
